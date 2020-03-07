eabd184679a5fc45ea34f4a06b78d08a753d34a9
ArtemVeshkin <artem.veshkin@gmail.com>
2020-03-07 11:10:21 +0300

    include.h

---
+++ include.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+namespace numbers
+{
+    class complex
+    {
+        double re_, im_;
+
+        void test_stream(std::stringstream &stream, char c)
+        {
+            if (stream.peek() != c)
+                {
+                    std::stringstream ex;
+                    ex << "expected /, but has: " << static_cast<char>(stream.peek());
+                    throw std::runtime_error(ex.str());
+                }
+        }
+    public:
+        complex(const double re = 0., const double im = 0.) : re_(re), im_(im) {}
+
+        explicit complex(std::string str) : re_(0), im_(0)
+        {
+            std::stringstream stream{str};
+            try
+            {
+                test_stream(stream, '(');
+                stream.ignore(1);
+                stream >> re_;
+                test_stream(stream, ',');
+                stream.ignore(1);
+                stream >> im_;
+            }
+            catch(const std::exception& ex)
+            {
+                std::cerr << ex.what() << std::endl;
+            }
+            
+        }
+
+        double re() const { return re_; }
+
+        double im() const { return im_; }
+
+        double abs() const
+        {
+            return sqrt(re_ * re_ + im_ * im_);
+        }
+
+        double abs2() const
+        {
+            return re_ * re_ + im_ * im_;
+        }
+
+        std::string to_string() const
+        {
+            size_t size = 256;
+            char buf[size];
+            snprintf(buf, size, "(%.10g,%.10g)", re_, im_);
+            return std::string(buf);
+        }
+
+        complex operator+=(const complex val)
+        {
+            return complex(re_ + val.re_, im_ + val.im_);
+        }
+
+        complex operator-=(const complex val)
+        {
+            return complex(re_ - val.re_, im_ - val.im_);
+        }
+
+        complex operator*=(const complex val)
+        {
+            return complex(re_ * val.re_ - im_ * val.im_,
+                re_ * val.im_ + im_ * val.re_);
+        }
+
+        complex operator/=(const complex val)
+        {
+            double k = val.re_ * val.re_ + val.im_ * val.im_;
+            return complex((re_ * val.re_ + im_ * val.im_) / k,
+                    (-re_ * val.im_ + im_ * val.re_) / k);
+        }
+
+        complex operator~() const
+        {
+            return complex(re_, -im_);
+        }
+
+        complex operator-() const
+        {
+            return complex(-re_, -im_);
+        }
+
+        friend complex operator+ (const complex lhs, const complex rhs);
+        friend complex operator- (const complex lhs, const complex rhs);
+        friend complex operator* (const complex lhs, const complex rhs);
+        friend complex operator/ (const complex lhs, const complex rhs);
+    };
+
+    complex operator+ (complex lhs, const complex rhs)
+    {
+        return (lhs += rhs);
+    }
+
+    complex operator- (complex lhs, const complex rhs)
+    {
+        return (lhs -= rhs);
+    }
+
+    complex operator* (complex lhs, const complex rhs)
+    {
+        return (lhs *= rhs);
+    }
+
+    complex operator/ (complex lsh, const complex rhs)
+    {
+        return (lsh /= rhs);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "include.h"
+
 namespace numbers
 {
     class complex_stack
@@ -28,15 +30,16 @@ namespace numbers
             return data_[size_];
         }
     public:
-        complex_stack() : size_(0), capacity_(1) {
+        complex_stack() : size_(0), capacity_(1), data_(nullptr) {
             data_ = new complex[capacity_];
         }
 
-        complex_stack(size_t size) : size_(size), capacity_(size + 1) {
+        complex_stack(size_t size) : size_(size), capacity_(size + 1), data_(nullptr) {
             data_ = new complex[capacity_];
         }
 
-        complex_stack(const complex_stack& base) {
+        complex_stack(const complex_stack& base) : size_(0), capacity_(1), data_(nullptr)
+        {
             size_ = base.size();
             capacity_ = size_;
             data_ = new complex[size_];
@@ -48,13 +51,24 @@ namespace numbers
 
         ~complex_stack() { delete[] data_; }
 
+        complex_stack operator=(const complex_stack& base)
+        {
+            size_ = base.size();
+            capacity_ = size_;
+            data_ = new complex[size_];
+            for (size_t i = 0; i < size_; ++i)
+            {
+                data_[i] = base[i];
+            }
+        }
+
         size_t size() const { return size_; }
 
         complex& operator[](size_t index) const { return data_[index]; }
 
         complex_stack operator<<(const complex val) const
         {
-            complex_stack new_stack(*this);
+            complex_stack new_stack = *this;
             new_stack.push(val);
             return new_stack;
         }
@@ -63,9 +77,14 @@ namespace numbers
 
         complex_stack operator~() const
         {
-            complex_stack new_stack(*this);
+            complex_stack new_stack = *this;
             new_stack.pop();
             return new_stack;
         }
     };
 };
+
+int main()
+{
+    // numbers::complex_stack stack;
+}
