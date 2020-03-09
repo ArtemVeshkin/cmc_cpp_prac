d276bc881dcd711dda8f926e9fd4292bd95191a1
ArtemVeshkin <artem.veshkin@gmail.com>
2020-03-09 19:28:19 +0300

    correct complex_stack

--- 4_sem_prac/my_practice/complex_stack.cpp
+++ 4_sem_prac/my_practice/complex_stack.cpp
@@ -5,56 +5,75 @@ namespace numbers
         size_t size_, capacity_;
         complex *data_;
 
-        void push(complex val)
+        void push(const complex& val)
         {
             if (capacity_ == size_)
             {
                 capacity_ *= 2;
-                complex *new_data = new complex[capacity_];
+                complex *new_data = static_cast<complex*> (
+                    ::operator new (capacity_ * sizeof(*new_data)));
                 for (size_t i = 0; i < size_; ++i)
                 {
                     new_data[i] = data_[i];
                 }
-                delete[] data_;
+                ::operator delete(data_);
                 data_ = new_data;
             }
-            ++size_;
-            data_[size_ - 1] = val;
+            new(data_ + size_++) complex(val);
         }
 
-        complex pop()
-        {
-            --size_;
-            return data_[size_];
-        }
+        void pop() { (data_ + --size_)->~complex(); }
     public:
-        complex_stack() : size_(0), capacity_(1) {
-            data_ = new complex[capacity_];
-        }
-
-        complex_stack(size_t size) : size_(size), capacity_(size + 1) {
-            data_ = new complex[capacity_];
-        }
-
-        complex_stack(const complex_stack& base) {
-            size_ = base.size();
-            capacity_ = size_;
-            data_ = new complex[size_];
-            for (size_t i = 0; i < size_; ++i)
+        complex_stack(size_t size = 0) : size_(size), capacity_(size + 1), data_(nullptr) {
+            data_ = static_cast<complex*> (::operator new (capacity_ * sizeof(*data_)));
+            for (size_t i = 0; i < capacity_; ++i)
             {
-                data_[i] = base[i];
+                new(data_ + i) complex();
             }
         }
 
-        ~complex_stack() { delete[] data_; }
+        complex_stack(const complex_stack& base) : size_(base.size_),
+            capacity_(base.capacity_), data_(nullptr)
+        {
+            data_ = static_cast<complex*> (::operator new (capacity_ * sizeof(*data_)));
+            for (size_t i = 0; i < size_; ++i)
+            {
+                new(data_ + i) complex(base[i]);
+            }
+        }
+
+        ~complex_stack() { ::operator delete(data_); }
+
+        void operator=(const complex_stack& base)
+        {
+            if (&base == this) { return; }
+            
+            if (capacity_ < base.capacity_)
+            {
+                ::operator delete(data_);
+                size_ = base.size_;
+                capacity_ = base.capacity_;
+                data_ = static_cast<complex*> (::operator new (capacity_ * sizeof(*data_)));
+                for (size_t i = 0; i < capacity_; ++i)
+                {
+                    new(data_ + i) complex(base[i]);
+                }
+            } else {
+                size_ = base.size_;
+                for (size_t i = 0; i < capacity_; ++i)
+                {
+                    data_[i] = base.data_[i];
+                }
+            }
+        }
 
         size_t size() const { return size_; }
 
         complex& operator[](size_t index) const { return data_[index]; }
 
-        complex_stack operator<<(const complex val) const
+        complex_stack operator<<(const complex& val) const
         {
-            complex_stack new_stack(*this);
+            complex_stack new_stack = *this;
             new_stack.push(val);
             return new_stack;
         }
@@ -63,7 +82,7 @@ namespace numbers
 
         complex_stack operator~() const
         {
-            complex_stack new_stack(*this);
+            complex_stack new_stack = *this;
             new_stack.pop();
             return new_stack;
         }
