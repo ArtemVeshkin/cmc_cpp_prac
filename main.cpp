69f68707c9aefef08579b24f97a416ebd6bfd741
ArtemVeshkin <artem.veshkin@gmail.com>
2020-04-12 18:03:48 +0300

    FileWrapper

--- main.cpp
+++ main.cpp
@@ -2,22 +2,49 @@
 
 class FileWrapper
 {
-	FILE *f_;
+	FILE *f_ = nullptr;
+	int* count_ = nullptr;
 
 public:
-	FileWrapper(const char* filename)
+	FileWrapper(const char* filename) : f_(fopen(filename, "w")), count_(new int{1}) {}
+
+	FileWrapper(const FileWrapper& parent) : f_(parent.f_), count_(parent.count_)
 	{
-		f_ = std::fopen(filename, "w");
+		++(*count_);
+	}
+
+	FileWrapper(FileWrapper&& parent) : f_(parent.f_), count_(parent.count_)
+	{
+		parent.count_ = new int{1};
+		parent.f_ = nullptr;
 	}
 
 	~FileWrapper()
 	{
-		fclose(f_);
+		if (*count_ == 1)
+		{
+			fclose(f_);
+			delete count_;
+		} else {
+			--(*count_);
+		}
 	}
 
-	FILE* get()
+	FileWrapper operator=(const FileWrapper& parent)
 	{
-		return f_;
+		f_ = parent.f_;
+		count_ = parent.count_;
+		++(*count_);
+		return *this;
+	}
+	
+	FileWrapper operator=(FileWrapper&& parent)
+	{
+		f_ = parent.f_;
+		count_ = parent.count_;
+		parent.count_ = new int{1};
+		parent.f_ = nullptr;
+		return *this;
 	}
 
 	friend FileWrapper& operator<<(FileWrapper& out, const char& msg);
@@ -25,6 +52,6 @@ public:
 
 FileWrapper& operator<<(FileWrapper& out, const char& msg)
 {
-	fprintf(out.get(), "%c", msg);
+	fprintf(out.f_, "%c", msg);
 	return out;
 }
